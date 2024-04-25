0b14137984732397cc69de6993e33bcad9bd4cc2
maramilod <marammilod03@gmail.com>
2024-04-25 17:01:02 +0100

    add

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+hey
+"""
+
+
+def pascal_triangle(n):
+    """
+    Create a function that returns a list of lists of integers
+    """
+    if n <= 0:
+        return []
+
+    arr = [[1]]
+    for i in range(1, n):
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(arr[i-1][j-1] + arr[i-1][j])
+
+        new_row.append(1)
+        arr.append(new_row)
+
+    return arr
