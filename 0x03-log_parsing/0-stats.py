b2c10c45b00eb7dbb327abb6dcffd5c632c06da8
maramilod <marammilod03@gmail.com>
2024-05-16 18:29:07 +0100

    add

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,25 +7,36 @@ y
 import sys
 
 tsize = 0
-status_counts = {}
-try:
-    for line in sys.stdin:
-        try:
-            ipA, _, date, get, status, size = line.split()
-            status = int(status)
-            tsize += int(size)
-            if status in status_counts:
-                status_counts[status] += 1
-            else:
-                status_counts[status] = 1
-        except (ValueError, IndexError):
-            continue
+scc = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-        if len(sys.stdin.readlines()) % 10 == 0 or sys.stdin.tell() > 0:
-            print("File size: {}".format(tsize))
-            for status, count in sorted(status_counts.items()):
-                print("{}: {}".format(status, count))
-except KeyboardInterrupt:
-    print("File size: {}".format(tsize))
-    for status, count in sorted(status_counts.items()):
-        print("{}: {}".format(status, count))
+def process(line):
+    global tsize
+    l = line.split()
+    try:
+        status_code = int(l[-2])
+        file_size = int(l[-1])
+
+        if status_code in scc:
+            scc[status_code] += 1
+            tsize += file_size
+    except (IndexError, ValueError):
+        pass
+
+def print_f():
+    print(f"Total file size: File size: {tsize}")
+    for status_code, count in sorted(scc.items()):
+        if count > 0:
+            print(f"{status_code}: {count}")
+
+if __name__ == "__main__":
+    try:
+        i = 0
+        for line in sys.stdin:
+            process(line)
+            i += 1
+            if i % 10 == 0:
+                print_f()
+        print_f()
+    except KeyboardInterrupt:
+        print_f()
+        raise
