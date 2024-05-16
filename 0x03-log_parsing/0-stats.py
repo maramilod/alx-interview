e6ab9426503c79f7c9b99b4089144152ea42cca6
maramilod <marammilod03@gmail.com>
2024-05-16 10:29:36 +0100

    add

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,24 +7,25 @@ y
 import sys
 
 tsize = 0
-statusc = {}
+status_counts = {}
 try:
     for line in sys.stdin:
         try:
             ipA, _, date, get, status, size = line.split()
             status = int(status)
             tsize += int(size)
-
-            if  status in statusc:
-                statusc[status] += 1
+            if status in status_counts:
+                status_counts[status] += 1
             else:
-                statusc[status] = 1
-        except ValueError:
-            pass
+                status_counts[status] = 1
+        except (ValueError, IndexError):
+            continue
 
         if len(sys.stdin.readlines()) % 10 == 0 or sys.stdin.tell() > 0:
             print("File size: {}".format(tsize))
-            for status, count in stored(tatusc.items()):
+            for status, count in stored(status_counts.items()):
                 print("{}: {}".format(status, count))
 except KeyboardInterrupt:
-    print("\nInterrupted by user.")
+    print("File size: {}".format(tsize))
+    for status, count in storeid(status_counts.items()):
+        print("{}: {}".format(status, count))
